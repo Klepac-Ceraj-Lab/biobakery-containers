83c5fe48580c890434914269789395f25e686719
Guilherme Fabur Bottino <gfahurbottino@gmail.com>
2025-06-30 13:43:39 -0400

    added bash scripts for apptainer runs for MP3 and MP4

---
+++ bin/metaphlan-3-app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Wrapper for MetaPhlAn 3
+
+# Set default container directory if CONTAINER_HOME is not defined
+CONTAINER_HOME="${CONTAINER_HOME:-$HOME/containers}"
+DATABASE_HOME="${DATABASE_HOME:-$HOME/Databases}"
+
+exec apptainer exec --bind "$DATABASE_HOME":/databases "$CONTAINER_HOME/metaphlan-3.sif" "$@"
--- bin/metaphlan-4-app.sh
+++ bin/metaphlan-4-app.sh
@@ -3,6 +3,6 @@
 
 # Set default container directory if CONTAINER_HOME is not defined
 CONTAINER_HOME="${CONTAINER_HOME:-$HOME/containers}"
-DATABASE_HOME="${DATABASE_HOME:-$HOME/databases}"
+DATABASE_HOME="${DATABASE_HOME:-$HOME/Databases}"
 
-exec apptainer exec --bind "$PWD":/mnt "$CONTAINER_HOME/metaphlan-4.sif" "$@"
+exec apptainer exec --bind "$DATABASE_HOME":/databases "$CONTAINER_HOME/metaphlan-4.sif" "$@"
